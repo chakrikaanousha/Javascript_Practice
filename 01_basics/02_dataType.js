fa504a1a14002d44756477cc251df6c6ef0c74dc
Chakrika_Anousha_ <101462904+chakrikaanousha@users.noreply.github.com>
2024-02-19 06:09:20 +0000

    datatypes

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,23 @@
+//to use the newre version
+"use strict";
+
+//alert("hello") //can be used in the dom or browser not by nodejs
+//documentation: mdn web docs
+
+//DATATYPES:
+//1. number => 2 poer 53
+//bigInt
+//String =>" "
+//Boolean =>True/false
+//null => standalone value =>empty
+//undefined => not assigned sany value
+//symbol => unquiness 
+
+//to find the datatype:
+const id = 9876;
+console.log(typeof(id));//number
+console.log(typeof"hiranya");//string
+
+//*** interview perspective
+console.log(typeof undefined); //undefined
+console.log(typeof null);//object
