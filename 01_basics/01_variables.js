96df7fab2f4335d04ea481a09e0dbc3d93cecfb2
Chakrika_Anousha_ <101462904+chakrikaanousha@users.noreply.github.com>
2024-02-19 05:11:40 +0000

    upd2

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+//e-commerece website
+//const = not changebale parameter
+const accountId = 14567
+accountId = 2345
+let accountEmail = "hiran@gmail.com"
+var accountPswd = "12345"
+accCity = "Rjasthan"
+
