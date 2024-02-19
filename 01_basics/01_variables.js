b3f39aab2bda1d38dcd5de1071936f539302a4cc
Chakrika_Anousha_ <101462904+chakrikaanousha@users.noreply.github.com>
2024-02-19 05:54:47 +0000

    update

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,8 +1,22 @@
 //e-commerece website
 //const = not changebale parameter
 const accountId = 14567
-accountId = 2345
+//accountId = 2345 //not allowed
+
+//variable - var, let => mostly used let
+//prob with var => issue in block scope and functional scope i.e{}
+
 let accountEmail = "hiran@gmail.com"
 var accountPswd = "12345"
+//declaring a variable without any specification: XXX
 accCity = "Rjasthan"
+console.log(accountId)
+
+//what are changeable? - let, var
+//changing
+accountEmail = "hari@gmail.com"
+accountPswd = "1254"
+accCity = "Banglore"
+//printing all vlaueas at a time:
+console.table([accountId,accountEmail,accountPswd,accCity])
 
